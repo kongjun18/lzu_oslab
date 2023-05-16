b792cbda3f85d7fd7a23a57d05cd3489ba2a2fc8
Kong Jun <kongjun18@outlook.com>
2023-05-16 11:59:37 +0800

    build: put all the code into os/ directory

--- os/lab2/include/kdebug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KDEBUG_H__
-#define __KDEBUG_H__
-#include <stdarg.h>
-#include <stddef.h>
-#include <sbi.h>
-int kputs(const char *msg);
-int kprintf(const char *fmt, ...);
-void kputchar(char ch);
-#endif
